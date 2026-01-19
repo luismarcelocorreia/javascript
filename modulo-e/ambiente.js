768d6593bc74d40e35c702feb25be7b25cbe9b29
Luis Marcelo <luismarcelo.fogo@gmail.com>
2026-01-19 16:08:12 -0300

    Update ambiente.js
    aula 13

--- modulo-e/ambiente.js
+++ modulo-e/ambiente.js
@@ -11,7 +11,10 @@ while (c <= 6){
     console.log(`passo ${c}`)
     c++
 }/*
-/*console.log("Tudo bem?")
+
+
+/*
+console.log("Tudo bem?")
 console.log("Tudo bem?")
 console.log("Tudo bem?")
 console.log("Tudo bem?")
