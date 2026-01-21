7e7553f7ded7e3f36c7adc0784b288bfa8b26276
Luis Marcelo <luismarcelo.fogo@gmail.com>
2026-01-21 16:48:31 -0300

    testando dom

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,1 +1,9 @@
-alert("Olá!")
+let num = document.getElementById("num")
+//num.innerHTML = "OI"
+let res = document.getElementById("res")
+num.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        let n = Number(num.value)
+         res.innerHTML = `${n}`
+    }
+})
