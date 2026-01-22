98e5e56ebf37c00cf100b42bd191a20cbaa56cda
Luis Marcelo <luismarcelo.fogo@gmail.com>
2026-01-22 16:29:17 -0300

    DesafioTabuada

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,15 +1,17 @@
-let num = document.getElementById("num")
-let res = document.getElementById("res")
+let num = document.getElementById("num")//pega o input onde o usuário digita o número
+let res = document.getElementById("res")//pega o id para aparecer o resultado
 
-num.addEventListener("keydown", function (event) {
+num.addEventListener("keydown", function (event) {//faz um evento pra quando apertar uma tecla
 
-    if (event.key === "Enter") {
+    if (event.key === "Enter") {//verifica se a tecla Enter foi pressionada
 
-        let n = Number(num.value)
-        res.innerHTML = ""
+        let n = Number(num.value)//pega o valor digitado e transforma em número
 
-        for (let s = 0; s <= 10; s++) {
-            res.innerHTML += `${n} x ${s} = ${n * s}<br>`
+        res.innerHTML = ""//limpa quando o usuario for fazer outra conta
+
+        for (let s = 0; s <= 10; s++) {//repetição que vai do 0 até 10
+            res.innerHTML += `${n} x ${s} = ${n * s}<br>`//pega o valor digitado usa a repeticao, multiplica e mostra pro usuario
         }
     }
 })
+
