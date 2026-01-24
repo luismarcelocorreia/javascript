4aa228fd44f3e526a292cba3a4533f787baa9f12
Luis Marcelo <luismarcelo.fogo@gmail.com>
2026-01-24 01:40:00 -0300

    ContadorDeNumeros

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,1 +1,26 @@
-alert("Olá!")
+let ini = document.getElementById("ini")
+let fim = document.getElementById("fim")
+let pas = document.getElementById("pas")
+let res = document.getElementById("res")
+
+
+pas.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+
+        let inicio = Number(ini.value)
+        let final = Number(fim.value)
+        let passos = Number(pas.value)
+
+        res.innerHTML = ""
+        if (ini > 0) {
+            for (let c = inicio; c <= final; c += passos) {
+                res.innerHTML += (`${c} 👉`)
+            }
+        }
+        else if (inicio || final || passos <= 0) {
+            window.alert("Numero invalido digite outro")
+        }
+
+
+    }
+})
