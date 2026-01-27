06ba5f0581b6ed6521091849099faa86e459019b
Luis Marcelo <luismarcelo.fogo@gmail.com>
2026-01-27 10:41:24 -0300

    MelhorandoDynamicCounter

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,35 +1,32 @@
-let ini = document.getElementById("ini")
-let fim = document.getElementById("fim")
-let pas = document.getElementById("pas")
-let res = document.getElementById("res")
+function contar() {
+    let ini = document.getElementById("ini")
+    let fim = document.getElementById("fim")
+    let pas = document.getElementById("pas")
+    let res = document.getElementById("res")
 
 
-pas.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
+
+    let inicio = Number(ini.value)
+    let final = Number(fim.value)
+    let passos = Number(pas.value)
+
+    res.innerHTML = ""
     
-        let inicio = Number(ini.value)
-        let final = Number(fim.value)
-        let passos = Number(pas.value)
-
-        res.innerHTML = ""
-
-        if (inicio > 0) {
-            if (inicio <= final) {
-                for (let c = inicio; c <= final; c += passos) {
-                    res.innerHTML += (`${c} 👉`)
-                }
-            }
-            else if (inicio > final) {
-                for (let c = inicio; c >= final; c -= passos) {
-                    res.innerHTML += (`${c} 👉`)
-                }
+    if (inicio <= 0 || final <= 0 || passos <= 0) {
+       window.alert("Numero invalido! Digite um número maior que 0")
+   }
+    else {
+        if (inicio <= final) {
+            for (let c = inicio; c <= final; c += passos) {
+                res.innerHTML += (`${c} 👉`)
             }
         }
-        
-        else if (inicio <= 0 || final <= 0 || passos <= 0) {
-            window.alert("Numero invalido! Digite um número maior que 0")
+        else if (inicio > final) {
+            for (let c = inicio; c >= final; c -= passos) {
+                res.innerHTML += (`${c} 👉`)
+            }
         }
+    }
 
 
-        
-    }})
+}
