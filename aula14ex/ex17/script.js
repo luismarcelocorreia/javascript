e5bf2ccb7bd2d68cb0eed825349b48ae5be677b6
Luis Marcelo <luismarcelo.fogo@gmail.com>
2026-02-01 00:55:17 -0300

    DynamicCounterUltimoReajuste

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -6,31 +6,38 @@ function contar() {
 
 
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-       window.alert("Preencha todos os campos")
-   }else {
-    
+        res.innerHTML = "Impossivel Contar!"
+        //window.alert("Preencha todos os campos")
+    }else {
+    //res.innerHTML = ""
+    res.innerHTML = "Contando: <br>"
+        
     let inicio = Number(ini.value)
     let final = Number(fim.value)
     let passos = Number(pas.value)
 
+    
+    
     if(passos == 0){
         window.alert("Numero de passos invalido! Considerando passos 1")
         passos = 1
     }
 
-    res.innerHTML = ""
-    
-        if (inicio <= final) {
+    if (inicio <= final) {
+            //Contagem crescente
             for (let c = inicio; c <= final; c += passos) {
-                res.innerHTML += (`${c} 👉`)
+                res.innerHTML += (`${c} \u{1F449}`)
             }
         }
-        else if (inicio > final) {
-            for (let c = inicio; c >= final; c -= passos) {
-                res.innerHTML += (`${c} 👉`)
-            }
+
+    else if (inicio > final) {
+         //Contagem regressiva
+        for (let c = inicio; c >= final; c -= passos) {
+            res.innerHTML += (`${c} 👉`)
         }
     }
 
+    res.innerHTML += `\u{1F3C1}`
 
+    }
 }
