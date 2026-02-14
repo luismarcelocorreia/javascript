ec0260c33dca38c184e66571e9798c09ce188ffc
Luis Marcelo <luismarcelo.fogo@gmail.com>
2026-02-14 18:10:56 -0300

    Tabuada

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,17 +1,21 @@
-let num = document.getElementById("num")//pega o input onde o usuário digita o número
-let res = document.getElementById("res")//pega o id para aparecer o resultado
+function tabuada(){
 
-num.addEventListener("keydown", function (event) {//faz um evento pra quando apertar uma tecla
+let num = document.getElementById("num")
+let res = document.getElementById("res")
 
-    if (event.key === "Enter") {//verifica se a tecla Enter foi pressionada
-
-        let n = Number(num.value)//pega o valor digitado e transforma em número
-
-        res.innerHTML = ""//limpa quando o usuario for fazer outra conta
-
-        for (let s = 0; s <= 10; s++) {//repetição que vai do 0 até 10
-            res.innerHTML += `${n} x ${s} = ${n * s}<br>`//pega o valor digitado usa a repeticao, multiplica e mostra pro usuario
-        }
+if (num.value.length == 0){
+    window.alert("Por favor, digite um número!!!")
+}
+else{
+    let n = Number(num.value)
+    let c = 1
+    res.innerHTML = ""
+    while(c <= 10){
+        let item = document.createElement("option")
+        item.text = `${n} x ${c} = ${n*c}`
+        res.appendChild(item)
+        c++
     }
-})
+}
 
+}
