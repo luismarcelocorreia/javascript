c76eeae0d7cf0e120f087ddd43b97ae3be4a7f90
Luis Marcelo <luismarcelo.fogo@gmail.com>
2026-09-02 20:56:34 -0300

    Aprendendo JS

--- aula11/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é ${vel}km/h`)
-if(vel > 60){
-    console.log("Você altrapassou o limite de velocidade")
-}
-console.log("Dirija sempre usando sinto de segurança")
--- aula14/ambiente.js
+++
@@ -1,5 +0,0 @@
-console.log("Vai começar...")
-for(var c = 1;c <= 4;c++){
-    console.log(c)
-}
-console.log("FIM!")
